e6eeaeb3b58ec003613b4439bf09d56ec877c1db
hielf <wercub@tom.com>
2013-09-18 15:49:03 +0800

    commit;

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,8 @@
 root = "/home/root/apps/bj_crm/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
+stderr_path "#{root}/log/err.log"
+stdout_path "#{root}/log/out.log"
 
 listen "/tmp/unicorn.bj_crm.sock"
 worker_processes 2
